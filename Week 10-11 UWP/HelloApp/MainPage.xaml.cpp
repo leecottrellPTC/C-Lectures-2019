a65730c60ad8df9d486b139cc0ab31605c20ccdd
Lee Cottrell <cottrell.lee@ptcollege.edu>
2019-06-17 15:26:46 -0400

    UWP app 1
    The hello application is posted.

---
+++ Week 10-11 UWP/HelloApp/MainPage.xaml.cpp
@@ -0,0 +1,40 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <string>
+using namespace std;
+
+using namespace HelloApp;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void HelloApp::MainPage::DisplayButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	String^ name = nameBox->Text;
+	String^ greeting = "Hello " + name;
+	//String^ is the reference class for string
+	
+	outputBlock->Text = greeting;
+
+}
